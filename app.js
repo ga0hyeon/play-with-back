16a27b80c25b2dacae2a4fc9b26f642726f96974
Hekatons <grabbit575@gmail.com>
2021-10-10 01:12:25 +0900

    DB init setting

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // node_modules 에 있는 express 관련 파일을 가져온다.
-var express = require('express')
+const express = require("express");
 
 // express 는 함수이므로, 반환값을 변수에 저장한다.
-var app = express()
+const app = express();
 
-// 3000 포트로 서버 오픈
-app.listen(3000, function() {
-    console.log("start! express server on port 3000")
-})
+app.get("/", (req, res) => res.send("Hello Play-With!!"));
+
+// 8080 포트로 서버 오픈
+app.listen(8081, function () {
+  console.log("start! express server on port 8081");
+});
